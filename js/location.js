d329f4b0ea8df9451e4b8482eb023df9630ee13f
jy <jcodeofficer@gmail.com>
2021-09-09 02:10:13 +0900

    마커 제거 기능 삭제 및 생성된 마커에 따른 폴리라인 생성

--- js/location.js
+++ js/location.js
@@ -1,20 +1,43 @@
+//***2021-09-08 
+
+//Auth
 const CLIENT_ID = "mh2xhg862g";
 const CLIENT_SECRET = "n6YVWkaxZMNHYq9DEkwOjx33MhS1ML6cWkNjIGvB";
 
-const CL = "current location";
+//DB 
+const STOAGE_DATA_KEY = "SAVED LOCATION";
+
+//HTML Element 
 const saveButton = document.querySelector('#saveLocation');
 const locationList = document.querySelector('#location-list');
-let currentLocation = [];
 
-let lat,lng;
-let map;
+// js 
+let currentLocation = [];  //local storage에 보낼 array
+let lat,lng; //geolocation으로 따온 lat,lng 저장할 공간
+let map; //map이 init되면 담겨질 지도객체
+let paths = [];
+
+function ccc(location) {
+    const l = new naver.maps.LatLng(location.lat, location.lng);
+    paths.push(l);
+
+}
+
 
 function saveLoc() {
-    localStorage.setItem(CL, JSON.stringify(currentLocation));
+    localStorage.setItem(STOAGE_DATA_KEY, JSON.stringify(currentLocation));
+
+    currentLocation.forEach(ccc);
+
+
+    const polyline = new naver.maps.Polyline({
+        map: map,
+        path: paths
+    });
 }
 
 function loadLoc() {
-    const savedLoc = localStorage.getItem(CL);
+    const savedLoc = localStorage.getItem(STOAGE_DATA_KEY);
 
     if(savedLoc != null) {
         const parsedLoc = JSON.parse(savedLoc);
@@ -24,22 +47,15 @@ function loadLoc() {
     }
 }
 
-function paintLoc(l) {
+function paintLoc(location) {
     const li = document.createElement('li');
-    li.innerText = `${l.lat} / ${l.lng }`;
+    li.innerText = `${location.lat} / ${location.lng }`;
     locationList.appendChild(li);
 
-    console.log(map);
-
     const marker = new naver.maps.Marker({
-        position: new naver.maps.LatLng(l.lat, l.lng),
+        position: new naver.maps.LatLng(location.lat, location.lng),
         map: map,
     });
-    const tmpl = new naver.maps.LatLng(l.lat,l.lng);
-    
-    marker.setPosition(tmpl);
-    console.log('paint');
-    
 }
 
 function initMap() {
@@ -48,11 +64,16 @@ function initMap() {
     map.setCenter(loc);
     
     naver.maps.Event.addListener(map, 'click', function(e) {
+        //클릭시 그 위치에 마커 생성 이벤트
         const marker = new naver.maps.Marker({
             map: map,
         });
         marker.setPosition(e.latlng);
 
+        // 그 위치의 행정주소 얻어오기
+        reverseGeocodingApi(e.latlng._lat,e.latlng._lng,'getMarkerLocation');
+        
+        //(위도경도)객체로 묶어서 저장시키기
         const tmploc = {
             "lat":e.latlng._lat,
             "lng":e.latlng._lng
@@ -60,18 +81,26 @@ function initMap() {
 
         currentLocation.push(tmploc);
     });
-
 }
 
-// request callback function
-function parse(data) {
+// reverse geocoding callback function -1 : get current location
+function getCurrentLocation(data) {
+
+    const currentregion = data['results'][0]['region'];
+    const r1 = `${currentregion['area1']['name']} ${currentregion['area2']['name']} ${currentregion['area3']['name']}`;
+    
+    const location = document.querySelector('#location');
+    location.innerText = r1;
+}
+
+// reverse geocoding callback function -2 : get marker location 
+function getMarkerLocation(data) {
     const region = data['results'][0]['region'];
     const r = `${region['area1']['name']} ${region['area2']['name']} ${region['area3']['name']}`;
-    const location = document.querySelector('#location');
-    location.innerText = r;
+
 }
-function jqueryAjaxRequest(lat,lng) {
-    const url = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lng},${lat}&orders=admcode,legalcode,addr,roadaddr&output=json&sourcecrs=epsg:4326&callback=parse`;
+function reverseGeocodingApi(lat, lng, callback) {
+    const url = `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?request=coordsToaddr&coords=${lng},${lat}&orders=admcode,legalcode,addr,roadaddr&output=json&sourcecrs=epsg:4326&callback=${callback}`;
     
     const settings = {
         url: url,
@@ -93,8 +122,12 @@ function onGeoOk(position) {
     lat = position.coords.latitude;
     lng = position.coords.longitude;
     
-    //구한 좌표로 reverse geocoding 통신
-    jqueryAjaxRequest(lat,lng);
+    //현 위치 받아오기
+    reverseGeocodingApi(lat,lng,'getCurrentLocation');
+    //지도시작
+    initMap();
+    //저장된 위치 불러오기
+    loadLoc();
 
 }   
 function onGeoError() {
@@ -104,7 +137,8 @@ function onGeoError() {
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
 saveButton.addEventListener('click', saveLoc);
-loadLoc();
+
+
 
 
 
